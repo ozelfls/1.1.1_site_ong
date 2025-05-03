a332acf6ad2c19d01417255508034575a2b6d7ad
ozelfls <danieloliveirats@gmail.com>
2025-05-02 23:30:32 -0300

    Co-authored-by: pedro-art2611 <pedro-art2611@users.noreply.github.com> Co-authored-by: Pedro Augusto <peugusto@users.noreply.github.com>

--- script/app.js
+++ script/app.js
@@ -9,3 +9,22 @@ window.addEventListener("scroll", () =>{
         logoCasa.src = './imagens/casaRaraBranco_1.png'
     }
 })
+
+const slides = document.querySelectorAll('.slide');
+  let indexAtual = 0;
+
+  function mostrarSlide(index) {
+    slides.forEach((slide, i) => {
+      slide.classList.remove('ativa');
+      if (i === index) slide.classList.add('ativa');
+    });
+  }
+
+  function mudarSlide(direcao) {
+    indexAtual += direcao;
+    if (indexAtual < 0) indexAtual = slides.length - 1;
+    if (indexAtual >= slides.length) indexAtual = 0;
+    mostrarSlide(indexAtual);
+  }
+  
+  mostrarSlide(indexAtual);
